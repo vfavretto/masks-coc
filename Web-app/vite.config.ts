7a03a5dafa5984f4417280beb4ef4467783e42d9
vfavretto <vf.favretto@gmail.com>
2025-01-08 14:49:44 -0300

    fix: vite.config to deploy

--- Web-app/vite.config.ts
+++ Web-app/vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/nome-do-repo/',
+  base: '/masks-coc/',
   plugins: [react()],
 });
